533103f57c99958a32952c8fcf249a6db615877e
jradnan <mohd.yasin085208@gmail.com>
2022-07-27 23:56:18 +0600

    node running

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const express = require('express')
+const mongoose = require('mongoose')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const cors = require('cors')
+const app = express()
+app.use(express.json())
+const PORT = process.env.PORT || 5000
+app.use(bodyParser.json())
+app.use(cors())
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.viapzy5.mongodb.net/?retryWrites=true&w=majority`;
+
+
+mongoose.connect(uri , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Database Is Connected'))
+    .catch((err)=> console.log(err))
+
+
+app.use('/users' , require('./Routes/usersRouter'))
+app.use('/product' , require('./Routes/productRoute'))
+app.use('/order' , require('./Routes/orderRouter'))
+app.use('/review' , require('./Routes/reviewsRouter'))
+app.use('/profile' , require('./Routes/profileRouter'))
+app.use('/payment' , require('./Routes/paymentRouter'))
+
+
+app.listen(PORT, () => {
+    console.log('Example app listening')
+})
